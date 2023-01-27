628f86827fb53e2ff7e07df4108e36c184baf0ea
AlinaTrukhina <alina.trukhina@gmail.com>
2023-01-26 21:43:10 -0600

    index.js file

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ const app = require('express')();
 app.get('/', (req, res) => {
     res.json({message: 'you are using Docker! Woot 🥇'})
 });
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
